62b4bf3e29809246368b369eac5610f26b46eac6
Pallavi Chapla <pallavi.chapla@gmail.com>
2020-05-18 22:02:06 +0530

    base related changes added

--- edm/src/assets/js/edm.js
+++ edm/src/assets/js/edm.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".menu_navigation").click(function () {
         debugger
         $(".admin_base").toggleClass("admin_mini_menu");
+        // $("body").toggleClass("menu_open");
     });
 
     $(".mobile_search").click(function () {
@@ -12,11 +13,14 @@ $(document).ready(function () {
         $(".mobile_search_section").hide();
     });
 
-    if (window.matchMedia("(max-width: 850px)").matches) {
-        $(".admin_base").addClass("admin_mini_menu");
-    } else {
-        $("admin_base").removeClass("admin_mini_menu");
-    }
+    // if (window.matchMedia("(max-width: 850px)").matches) {
+    //     $(".admin_base").removeClass("admin_mini_menu");
+    //     $("body").removeClass("menu_open");
+    // } else {
+    //     $(".admin_base").addClass("admin_mini_menu");
+    //     $("body").addClass("menu_open");
+    // }
+
     $(".open_mobile_menu").click(function () {
         $(".mobile_menu").show();
     });
