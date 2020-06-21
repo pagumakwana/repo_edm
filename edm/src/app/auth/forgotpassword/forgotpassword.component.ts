37b0afc0990f1bdc915de72f57493cdfda4ca84a
Pallavi Chapla <pallavi.chapla@gmail.com>
2020-06-21 21:27:03 +0530

    Forgot Password page design as per updated wireframe

--- edm/src/app/auth/forgotpassword/forgotpassword.component.ts
+++ edm/src/app/auth/forgotpassword/forgotpassword.component.ts
@@ -3,13 +3,12 @@ import { BaseServiceHelper } from 'src/app/_appService/baseHelper.service';
 @Component({
     selector: 'app-forgotpassword',
     templateUrl: './forgotpassword.component.html',
-    styleUrls: ['./forgotpassword.component.scss']
+    styleUrls: ['../login/login.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
 
     constructor(private _base: BaseServiceHelper) { }
     ngOnInit(): void {
     }
-
 
 }
