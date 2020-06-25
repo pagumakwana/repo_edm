184b19774f31459da2a17c535895da1851368b05
Pallavi Chapla <pallavi.chapla@gmail.com>
2020-06-25 22:07:26 +0530

    design changes added

--- edm/src/app/profiles/producer/producer.component.ts
+++ edm/src/app/profiles/producer/producer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
     selector: 'app-producer',
     templateUrl: './producer.component.html',
-    styleUrls: ['./producer.component.scss','../../widgets/featuredProductSlider/featuredProductSlider.component.scss'],
+    styleUrls: ['./producer.component.scss','../../widgets/featuredProductSlider/featuredProductSlider.component.scss','../../home/home.component.scss','../../service/service.component.scss','../../product/product.component.scss'],
     encapsulation: ViewEncapsulation.None,
 })
 
