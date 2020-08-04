9348d24df33f9a154f3dc3a16aa59fa266002670
rawshan-more <roshanmore2905@gmail.com>
2020-08-04 13:32:24 +0530

    routing changes

--- edm/src/app/auth/forgotpassword/forgotpassword.component.ts
+++ edm/src/app/auth/forgotpassword/forgotpassword.component.ts
@@ -7,9 +7,15 @@ import { BaseServiceHelper } from 'src/app/_appService/baseHelper.service';
     encapsulation: ViewEncapsulation.None,
 })
 export class ForgotPasswordComponent implements OnInit {
-
+    screenSwitch: any = "Authenticate";
     constructor(private _base: BaseServiceHelper) { }
     ngOnInit(): void {
     }
 
+    public changeScreen(flag: any) {
+        if(flag == "OTP")
+            this.screenSwitch = "OTP";
+        else if(flag == "Reset")
+            this.screenSwitch = "Reset";
+    }
 }
