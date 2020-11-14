6748c80404449ba7e1c8229f5aed98c388a5ca5a
Amey Ranavade <47138480+ameyranavade@users.noreply.github.com>
2020-11-14 15:16:19 +0530

    Implemented checkbox in data table

--- edm/src/app/commonmodule/datatables/datatables.module.ts
+++ edm/src/app/commonmodule/datatables/datatables.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DatatablesComponent } from './datatables.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [
         DatatablesComponent,
     ],
     imports: [
         CommonModule,
+        FormsModule
         // RouterModule.forChild([
         //     {
         //         path: '',
